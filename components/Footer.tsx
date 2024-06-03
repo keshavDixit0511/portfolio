a7fd21225f58968afe3956157d2aa1d35598aecc
keshavDixit0511 <keshavd352@gmail.com>
2024-06-04 00:42:39 +0530

    portfolio

---
+++ components/Footer.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className='w-full pt-20 pb-10' id='contact'>
+        <div>
+            <img 
+                src="/footer-grid.svg" 
+                alt="" />
+        </div>
+    </footer>
+  )
+}
+
+export default Footer
